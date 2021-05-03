2ed715cc8526c3753ca2c12106262556c60d1cfc
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-02 19:34:22 -0500

    user controller CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,34 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        if user.update(user_params)
+            render json: user
+        else 
+            render json: { error: 'failed to update user account' }, status: :not_acceptable
+        end 
+    end 
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy
+        render json: {message: 'account has been deleted'}
+    end 
+    
+      private
+    
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end  
+
 end
