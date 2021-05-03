a69427c03cf7db004e32454ad0bc9b7e666786a5
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-02 19:39:58 -0500

    signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :goals
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/signup', to: 'users#create'
+  #post '/login', to: 'auth#create'
 end
