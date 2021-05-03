a2d7e67555c6ffb30f24b4b7cafc5739d6b3eba9
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-02 19:38:21 -0500

    goal controller CRUD

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,5 +8,35 @@ class GoalsController < ApplicationController
         goal = Goal.find_by(id: params[:id])
         render json: goal
     end
+
+    def create
+        goal = Goal.create(goal_params)
+        if goal.valid?
+            render json: goal
+        else
+            render json: { error: 'failed to create goal' }, status: :not_acceptable
+        end
+    end
+
+    def update
+        goal = Goal.find_by(id: params[:id])
+        if goal.update(goal_params)
+            render json: goal
+        else 
+            render json: { error: 'failed to update goal' }, status: :not_acceptable
+        end 
+    end 
+
+    def destroy
+        goal = Goal.find_by(id: params[:id])
+        goal.destroy
+        render json: {message: 'goal has been deleted'}
+    end 
     
+      private
+    
+    def goal_params
+        params.require(:goal).permit(:description, :user_id)
+    end  
+
 end
