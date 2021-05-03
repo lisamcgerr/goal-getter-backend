b5b12af391696a32e5081b11d7227281460e0bf9
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-02 19:16:29 -0500

    first migration and seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,26 @@
 User.destroy_all
 Goal.destroy_all
 
-User.create(username: )
+User.create(username: 'lisas_learning', 
+            password: 'coding' )
+
+Goal.create(
+        description: 'Train for Half Marathon - 2 hours 2 minute goal time',
+        days: 2,
+        user_id: 1
+)
+
+Goal.create(
+    description: 'Complete 100 days of Coding',
+    days: 0,
+    user_id: 1
+)
+
+Goal.create(
+    description: 'Study Italian for 30 minutes a day',
+    days: 10,
+    user_id: 1
+)
 
 puts "after successfully seeding..."
 puts "#{User.all.count} users"
