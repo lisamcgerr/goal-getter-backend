f10552991c19b3fa8510bf871eb7d87f78ebecf8
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-05-02 19:02:54 -0500

    added user & goal migrations

---
+++ db/migrate/20210503000227_create_goals.rb
@@ -0,0 +1,12 @@
+class CreateGoals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :goals do |t|
+      t.string :description
+      t.integer :days
+      t.boolean :completed
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
